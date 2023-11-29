3c2d48627361c18b1eca35fbbaaa1d8c01f67fc3
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2023-11-29 13:11:39 -0400

    Modificando el Header para el modo oscuro

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import IconoCrear from './media/iconos/create.svg'
-
+import BotonModoOscuro from './ModOscuro';
 export function Header() {
     console.log('colocamos el header');
     return <>
         <header>
             <h1>To-Do List</h1>
+            <div><BotonModoOscuro/></div>
         </header>
     </>
 }
