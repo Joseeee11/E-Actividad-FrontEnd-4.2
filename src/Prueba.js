b20f7a16850e0c30fb8ea4fab6f0d878889395a5
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-11-29 01:31:01 -0400

    Estilos Principales del Sitio
    Paleta de Colores

--- src/Prueba.js
+++ src/Prueba.js
@@ -4,11 +4,13 @@ export function Prueba({tareas, setTareas}){
     console.log('estamos en prueba', tareas)
     return <>
         {tareas.map((cadaTarea, i) => (
-            <div key={i} >
-                <h1> {cadaTarea.titulo} <span> posicion {i} </span></h1>
-                <p> {cadaTarea.descripcion} </p>
-                <ButtonDelete posicion={i} tareas={tareas} setTareas={setTareas} />
-            </div>
+            <main>
+                <div key={i} >
+                    <h1> {cadaTarea.titulo} <span> posicion {i} </span></h1>
+                    <p> {cadaTarea.descripcion} </p>
+                    <ButtonDelete posicion={i} tareas={tareas} setTareas={setTareas} />
+                </div>
+            </main>
             ))
         }
     </>
