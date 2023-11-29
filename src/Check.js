d085b7ae9b989e809be8813a85187e287eb455e2
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-11-29 10:35:26 -0400

    Funcion para marcar como completado
    z-index, para corregir posicionado que provocaba errores en pantalla

--- src/Check.js
+++ src/Check.js
@@ -1,17 +1,18 @@
+export function ButtonCheck({posicion, tareas, setTareas}){
+    console.log(posicion)
+    return <button onClick={() => Check(posicion, tareas, setTareas)}> LISTA </button>
+}
 
+function Check(posicion, tareas, setTareas) {
+    console.log('damos check');
+    console.log(posicion)
+    let nuevasTareas = [...tareas]; // Crea una copia de las tareas
+    // nuevasTareas[posicion] = {...nuevasTareas[posicion], estado: true};
+    nuevasTareas[posicion].estado = true;
+    console.log(nuevasTareas)
+    setTareas(nuevasTareas) // Actualiza la lista de tareas
+}
 
-// export function ButtonCheck({posicion, tareas, setTareas}){
-
-//     console.log('damos check');
-
-//     // function Check() {
-//     //     console.log('listaaa')
-//     //     console.log(tareas)
-//     //     const nuevasTareas = tareas; // Crea una copia de las tareas
-//     //     nuevasTareas[posicion].check = true;
-//     //     console.log(nuevasTareas)
-//     //     setTareas(nuevasTareas) // Actualiza la lista de tareas
-//     // }
-
-//     // return <button onClick={console.log('check')}> Lista? </button>
-// }
+//colocar en renderizar
+// <ButtonCheck posicion={i} tareas={tareas} setTareas={setTareas} />
+//import { ButtonDelete } from './Delete'
