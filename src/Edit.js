fe76c58a834feeb9a18a29fdcedd9acf644e9ef3
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-11-29 17:39:30 -0400

    Editar Tareas

---
+++ src/Edit.js
@@ -0,0 +1,89 @@
+import pencilIMG from "./media/iconos/pencil-solid.svg";
+import React, { useState } from "react";
+import TaskForm from "./Crear";
+export function ButtonEditar({ posicion, tareas, setTareas }) {
+
+    const [editando, setEditando] = useState(false)
+    const [titulo, setTitulo] = useState(tareas[posicion].titulo);
+    const [descripcion, setDescripcion] = useState(tareas[posicion].descripcion);
+    const [importance, setImportance] = useState(tareas[posicion].importance);
+    const [date, setDate] = useState(tareas[posicion].date);
+    console.log(titulo,descripcion,importance,date);
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      switch (name) {
+        case 'titulo':
+          setTitulo(value);
+          break;
+        case 'descripcion':
+          setDescripcion(value);
+          break;
+        case 'importance':
+          setImportance(value);
+          break;
+        case 'date':
+          setDate(value);
+          break;
+        default:
+          break;
+      }
+    };
+    return (
+      <>
+        <button onClick={() => {Editar(editando,setEditando)}}>
+          <img src={pencilIMG} alt="Editar" />
+        </button>
+        ;
+        <div className={editando == true? 'editar-Abierto':'editar-Cerrado'}> 
+        <form>
+      <label>
+        Título:
+        <input type="text" name="titulo" value={titulo} onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Descripción:
+        <textarea name="descripcion" value={descripcion} onChange={handleChange}/>
+      </label>
+      <br />
+      <label>
+        Importancia:
+        <select name="importance" value={importance} onChange={handleChange}>
+          <option value="">Seleccionar</option>
+          <option value="IMPORTANTE">Importante</option>
+          <option value="URGENTE">Urgente</option>
+          <option value="NORMAL">Normal</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Fecha:
+        <input type="date" name="date" value={date} onChange={handleChange}/>
+      </label>
+      <br />
+      <button onClick={(evento)=>Guardar(evento,posicion,tareas,setTareas,titulo,descripcion,importance,date)}>Guardar tarea</button>
+    </form>
+        </div>
+      </>
+    );
+}
+
+function Guardar(evento,posicion,tareas,setTareas,titulo,descripcion,importance,date){
+    evento.preventDefault();
+    let devolver= [...tareas]
+    devolver[posicion].titulo=titulo
+    devolver[posicion].descripcion=descripcion
+    devolver[posicion].importance=importance
+    devolver[posicion].date=date
+    setTareas(devolver);
+}
+
+function Editar(editando,setEditando) {
+    if (editando==false) {
+        setEditando(true)
+    }else{
+        setEditando(false)
+    }
+    
+}
