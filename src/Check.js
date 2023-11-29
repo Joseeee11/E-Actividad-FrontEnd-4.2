dd8b66b22ae88c92fe726c929319fd915068c1ed
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-11-29 14:26:49 -0400

    Funcion Check Aplicada
    Añadido el boton para establecer si la tarea fue completada

--- src/Check.js
+++ src/Check.js
@@ -1,18 +1,22 @@
+import IconoCompletado from './media/iconos/circle-check-regular.svg'
+import IconoHacer from './media/iconos/circle-xmark-regular.svg'
+
 export function ButtonCheck({posicion, tareas, setTareas}){
     console.log(posicion)
-    return <button onClick={() => Check(posicion, tareas, setTareas)}> LISTA </button>
+    return <button onClick={() => Check(posicion, tareas, setTareas)}> <img src={tareas[posicion].estado === true ? IconoHacer : IconoCompletado} /> </button>
 }
 
 function Check(posicion, tareas, setTareas) {
     console.log('damos check');
     console.log(posicion)
-    let nuevasTareas = [...tareas]; // Crea una copia de las tareas
-    // nuevasTareas[posicion] = {...nuevasTareas[posicion], estado: true};
-    nuevasTareas[posicion].estado = true;
+    let nuevasTareas = [...tareas]; // variable nueva con las tareas
+    if (nuevasTareas[posicion].estado === true) {
+        nuevasTareas[posicion].estado = false;
+    } else {
+        nuevasTareas[posicion].estado = true;
+    };
     console.log(nuevasTareas)
-    setTareas(nuevasTareas) // Actualiza la lista de tareas
+    setTareas(nuevasTareas) // actualizar la lista de tareas
 }
 
-//colocar en renderizar
-// <ButtonCheck posicion={i} tareas={tareas} setTareas={setTareas} />
-//import { ButtonDelete } from './Delete'
+
--- src/Renderizar.js
+++ src/Renderizar.js
@@ -1,9 +1,9 @@
 import imagenOso from "./media/OsoLista.png";
 import { ButtonDelete } from './Delete'
-
+import { ButtonCheck } from './Check'
 
 export function RenderizarTodas({tareas, setTareas}) {
-    if (tareas.length === 0 || !tareas) {
+    if (!tareas || tareas.length === 0) {
       return (
         <>
           <img src={imagenOso} alt="Oso con una lista" />
@@ -13,17 +13,17 @@ export function RenderizarTodas({tareas, setTareas}) {
     } else {
       return <>
           {tareas.map((tarea, i) => (
-            <div key={i} >
-              <div>
-                <p>{tarea.date}</p>
-                <button type="button"></button>
+            <div key={i} className="tarea">
+              <div className="fecha-check">
+                <p className={tarea.estado === true ? "tachado" : ""} >{tarea.date}</p>
+                <ButtonCheck posicion={i} tareas={tareas} setTareas={setTareas} />
               </div>
-              <div>
-                <h2>{tarea.titulo}</h2>
-                <p>Descripción: {tarea.descripcion}</p>
+              <div className="titulo-descripcion">
+                <h2 className={tarea.estado === true ? "tachado" : ""} >{tarea.titulo}</h2>
+                <p>Detalles: {tarea.descripcion}</p>
               </div>
-              <div>
-                <h3>{tarea.importance}</h3>
+              <div className="importancia-botones">
+                <h3 className={tarea.importance === 'IMPORTANTE' ? 'importante' : tarea.importance === 'URGENTE' ? 'urgente' : 'normal'} >{tarea.importance}</h3>
                 <div>
                   <ButtonDelete posicion={i} tareas={tareas} setTareas={setTareas} />
                   <button>edit</button>
